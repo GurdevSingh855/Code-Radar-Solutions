dfb6d89fe53f9d27297f28ccc2b5b9eb7c9ba2d1
GurdevSingh855 <gurdev0294.becse24@chitkara.edu.in>
2025-02-24 15:31:23 +0530

    Solution to Check Profit or Loss

--- Check Profit or Loss@@6788dd63490637cfc44e1b98/code.c
+++ Check Profit or Loss@@6788dd63490637cfc44e1b98/code.c
@@ -11,4 +11,4 @@ else if(s-c == 0){
 }
 else{
     printf("Loss");
-} return 0; }                                                                                                                                                                                                                                                                                                                                                                                                                                        1
+} return0; }                                                                                                                                                                                                                                                                                                                                                                                                                                        1
